4966f74926cd48e5338a9f94c713955e86932f35
Jacob <jacobharryloeser@github.com>
2017-11-28 15:26:20 -0500

    SGD implementation complete, but needs checking

--- sgd/SGD.java
+++ sgd/SGD.java
@@ -10,24 +10,50 @@ public class SGD {
         learner randomly permuted, @epochs is the hyperparameter controlling the
         number of iterations to run SGD, @l_rate is the hyperparameter controlling
         learning rate, @dim is the dimensionality of the problem
+        
+        TODO: Data types may not be correct. For example return type should probably
+        be double array since we're working with parameter vectors.
+        TODO: Check to see if this gradient is correct analytically.
+        Source: https://stats.stackexchange.com/questions/219241/gradient-for-logistic-loss-function
+        
     */
-    public static double SGD(double[] samples, int epochs, double[] l_rate, int dim) {
+    public static double SGD(double[][] samples, int epochs, double l_rate, int dim) {
         //Initialize constraint vector
         double[] w = new double[dim];
         for(int i = 0; i < dim; i++) {
             w[i] = 0;
         }
         //Start training in epochs
-        for(int t = 0; i < epochs; t++) {
+        for(int t = 0; t < epochs; t++) {
             //Have G-Oracle Produce Random Vector =>
-            //Draw an example from samples and calculate logistic loss
-            //Random Vector = Gradient of the Loss
+            //Draw an example from samples and calculate gradient of logistic loss
+            
+            //Evaluate loss function at t(th) weight and fresh example
             double loss = logistic_loss(w[t], samples[t]);
+            //Gradient of the loss is difference between true label and loss
+            //This assumes that true label lies at 3 entry in each row of sample
+            double grad_loss = loss - samples[t][2];
+            
+            //Update Step
+            double value = update(w[t], loss, l_rate);
+            //Projection Step
+            w[t+1] = project(value);
         }
+        //Return Empirical Loss
+        double w_hat = 0;
+        for(int t = 0; t < epochs; t++) {
+            w_hat += w[t];
+        }
+        return (w_hat/epochs);
         
     }
+    //Loss Update
+    public static double update(double w_t, double err, double l_rate) {
+        return w_t-(l_rate*err);
+    }
+    //TODO: Finish Projection Function
     //Euclidean Projection
-    public static double[] project(double[] v) {
+    public static double project(double v) {
         return v;
     }
     //Logistic Loss Function
@@ -45,6 +71,7 @@ public class SGD {
         }
         return dot;
     }
+    //TODO: Test with actual data
     public static void main(String[] args) {
         //Do file I/O to get Sample Data
     }
