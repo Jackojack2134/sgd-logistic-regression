4339564e39ae17f5aa831cee1f0e6fad87aafab8
Jackojack2134 <jackojack2134@gmail.com>
2017-11-29 19:57:51 -0500

    Update SGD.java

--- sgd/SGD.java
+++ sgd/SGD.java
@@ -1,6 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException
+import java.io.UnsupportedEncodingException;
 import java.lang.Math;
 import java.io.BufferedReader;
 import java.io.FileReader;
